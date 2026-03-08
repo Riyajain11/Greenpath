91d30e838a0d8a1359aa16567f377e5b0297baed
Riya Jain <jriya9090@gmail.com>
2026-03-08 19:57:08 +0530

    Connect ML service with backend

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -3,29 +3,34 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/smart-price", async (req, res) => {
+
   const { cropType, state, expectedPricePerKg } = req.query;
 
   try {
+
     const response = await axios.get(
-      "https://greenpath-ml.onrender.com/predict",
+      "https://greenpath-1.onrender.com/predict",
       {
         params: {
           cropType,
           state,
-          expectedPricePerKg,
-        },
+          expectedPricePerKg
+        }
       }
     );
 
     res.json(response.data);
 
   } catch (error) {
+
     console.error("AI ERROR 👉", error.message);
+
     res.status(500).json({
       success: false,
-      error: "AI prediction failed",
+      error: "AI prediction failed"
     });
   }
+
 });
 
 module.exports = router;
